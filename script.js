62903789a2473567e4111fcc192eb7360cb20e7e
Conal Elliott <conal@conal.net>
2015-01-30 13:43:53 -0800

    load_shader

--- script.js
+++ script.js
@@ -1,87 +1,63 @@
-var main=function() {
-  var canvas=document.getElementById("your_canvas");
-//   canvas.width=window.innerWidth;
-//   canvas.height=window.innerHeight;
+function get_file_text(path) {
+    var XHR = new XMLHttpRequest();
+    XHR.open("GET", path, false);
+    if (XHR.overrideMimeType) {
+        XHR.overrideMimeType("text/plain");
+    }
+    try {
+        XHR.send(null);
+    } catch(e) {
+        this.println('Error reading file "' + path + '"');
+    }
+    if (!XHR.responseText)
+        throw ("get_file_text "+path+": file not found.");
+    return XHR.responseText;
+};
 
-  /*========================= GET WEBGL CONTEXT ========================= */
-  var gl;
-  try {
-    gl = canvas.getContext("experimental-webgl", {antialias: true});
-  } catch (e) {
-    alert("You are not webgl compatible :(") ;
-    return false;
-  }
-  // alert ("gl.SAMPLES == " + gl.getParameter(gl.SAMPLES));
+function get_ext(path) {
+    var ext = path.split('.').pop();
+    return (ext==path) ? '' : ext;
+}
 
-  /*========================= SHADERS ========================= */
-  /*jshint multistr: true */
-  var shader_vertex_source="\n\
-attribute vec2 position;\n\
-varying vec2 vPos;\n\
-\n\
-void main(void) {\n\
-gl_Position = vec4(position, 0., 1.);\n\
-vPos = position;\n\
-}";
+function load_shader(gl,path) {
+    var ext = get_ext(path),
+        content = get_file_text(path);
+    switch (ext) {
+    case "vert":
+        return get_shader(gl,content, gl.VERTEX_SHADER, "VERTEX");
+    case "frag":
+        return get_shader(gl,content, gl.FRAGMENT_SHADER, "FRAGMENT");
+    }
+}
 
-  var shader_fragment_source_1="\n\
-precision mediump float;\n\
-uniform float time;\n\
-varying vec2 vPos;\n\
-const vec3 red=vec3(1,0,0);\n\
-const vec3 blue=vec3(0,0,1);\n\
-const vec3 yellow=vec3(1,1,0);\n\
-const vec3 white=vec3(1,1,1);\n\
-\n\
-void main(void) {\n\
-gl_FragColor = vec4(mix(mix(blue,yellow,vPos.x),mix(white,red,vPos.x),mod(vPos.y+time,1.0)),1);\n\
-}";
-
-  var shader_fragment_source="\n\
-precision mediump float;\n\
-uniform float time;\n\
-varying vec2 vPos;\n\
-void main(void) {\n\
-  float c = cos(time), s = sin(time);\n\
-  vec2 w = vec2 (vPos.x * c - vPos.y * s, vPos.x * s + vPos.y * c);\n\
-  // bool b = w.x > 0.0;\n\
-  // bool b = length(vPos) < 0.5;\n\
-  bool b = length(vPos) < 0.5 ^^ w.x > 0.0;\n\
-  float q = b?1.0:0.0;\n\
-  gl_FragColor = vec4(q,q,q,1);\n\
-}";
-
-  var shader_fragment_source_a10="\n\
-precision mediump float;\n\
-uniform float time;\n\
-varying vec2 vPos;\n\
-void main () {\n\
-    float a = 1.0;\n\
-    float b = time;\n\
-    float c = sin(b);\n\
-    float d = 1.0 / c;\n\
-    float e = cos(b);\n\
-    vec2  g = vPos;\n\
-    float k = d * dot(vec2(e,c),g);\n\
-    float s = d * dot(vec2(e,- c),g.yx);\n\
-    gl_FragColor = vec4(a - k + s * (k + (-1.0 + k)),s * k,k,a);\n\
-}";
-
-  var shader_fragment_source_a10_loaded=getFileText("shaders/a10.frag");
-
-  var get_shader=function(source, type, typeString) {
+function get_shader(gl,source, type, typeString) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert("ERROR IN "+typeString+ " SHADER :\n" + gl.getShaderInfoLog(shader));
-      return false;
+        alert("ERROR IN "+typeString+ " SHADER :\n" + gl.getShaderInfoLog(shader));
+        return false;
     }
     return shader;
-  };
+};
 
-  var shader_vertex=get_shader(shader_vertex_source, gl.VERTEX_SHADER, "VERTEX");
-  var shader_fragment=get_shader(shader_fragment_source_a10_loaded, gl.FRAGMENT_SHADER, "FRAGMENT");
+function main() {
+  var canvas=document.getElementById("your_canvas");
+
+  /*========================= GET WEBGL CONTEXT ========================= */
+  var gl;
+  try {
+      gl = canvas.getContext("experimental-webgl", {antialias: true});
+  } catch (e) {
+      alert("You are not webgl compatible :(") ;
+      return false;
+  }
+  // alert ("gl.SAMPLES == " + gl.getParameter(gl.SAMPLES));
+
+  /*========================= SHADERS ========================= */
+
+  var shader_vertex=load_shader(gl,"shaders/image.vert");
+  var shader_fragment=load_shader(gl,"shaders/a10.frag");
 
   var SHADER_PROGRAM=gl.createProgram();
 
@@ -134,7 +110,7 @@ void main () {\n\
   var redraw=function(t) {
       if (_time) {
           gl.uniform1f(_time, t/1000);
-          queueDraw();
+          // queueDraw();
       }
       // gl.clear(gl.COLOR_BUFFER_BIT);
       gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
@@ -153,20 +129,4 @@ void main () {\n\
   }
   window.addEventListener('resize', resizeCanvas, false);
   resizeCanvas();
-};
-
-var getFileText = function(path) {
-    var XHR = new XMLHttpRequest();
-    XHR.open("GET", path, false);
-
-    if (XHR.overrideMimeType) {
-        XHR.overrideMimeType("text/plain");
-    }
-
-    try {
-        XHR.send(null);
-    } catch(e) {
-        this.println('Error reading file "' + path + '"');
-    }
-    return XHR.responseText;
 };
