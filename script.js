563742aa65224105b620077844c90e111c4c42eb
Conal Elliott <conal@conal.net>
2015-02-05 08:06:48 -0800

    indentation

--- script.js
+++ script.js
@@ -44,119 +44,120 @@ function get_shader(gl,source, type, typeString) {
     return shader;
 };
 
-function install_effect(canvas) {
-  var gl;
-  try {
-      gl = canvas.getContext("webgl", {antialias: true,alpha:false});
-  } catch (e) {
-      alert("You are not webgl compatible :(") ;
-      return false;
-  }
-  // alert ("gl.SAMPLES == " + gl.getParameter(gl.SAMPLES));
+function install_effect(canvas,effect) {
+    // console.log("install_effect " + effect);
+    var gl;
+    try {
+        gl = canvas.getContext("webgl", {antialias: true,alpha:false});
+    } catch (e) {
+        alert("You are not webgl compatible :(") ;
+        return false;
+    }
+    // alert ("gl.SAMPLES == " + gl.getParameter(gl.SAMPLES));
 
-  // Geometry
-  var TRIANGLE_VERTEX =  gl.createBuffer ();
-  gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
-  gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([-1,-1, 1,-1, 1,1, -1,1]),
-                gl.STATIC_DRAW);
-  var TRIANGLE_FACES =  gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array([0,1,2, 0,2,3]),
-                gl.STATIC_DRAW);
+    // Geometry
+    var TRIANGLE_VERTEX =  gl.createBuffer ();
+    gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
+    gl.bufferData(gl.ARRAY_BUFFER,new Float32Array([-1,-1, 1,-1, 1,1, -1,1]),
+                  gl.STATIC_DRAW);
+    var TRIANGLE_FACES =  gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array([0,1,2, 0,2,3]),
+                  gl.STATIC_DRAW);
 
-  // I want to experiment with blending for cheap temporal and
-  // spatial antialiasing.
-  gl.disable(gl.DEPTH_TEST);
-  gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  /*
-  gl.blendEquation( gl.FUNC_SUBTRACT );
-  */
-  gl.clearColor(1.0, 0.0, 0.0, 0.0);
+    // I want to experiment with blending for cheap temporal and
+    // spatial antialiasing.
+    /*
+    gl.disable(gl.DEPTH_TEST);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    */
+    gl.clearColor(1.0, 0.0, 0.0, 0.0);
 
-  var panX,panY;
-  var tweak_pan = function(moveX,moveY) { // in pixels
-//       console.log("pan was (" + panX + "," + panY + ")" );
-      panX += moveX * pixel_size;
-      panY += moveY * pixel_size;
-//       console.log("pan = (" + panX + "," + panY + ")" );
-      gl.uniform2f(_pan,panX,panY);
-      queue_draw();
-  }
+    var panX,panY;
+    var tweak_pan = function(moveX,moveY) { // in pixels
+        // console.log("pan was (" + panX + "," + panY + ")" );
+        panX += moveX * pixel_size;
+        panY += moveY * pixel_size;
+        // console.log("pan = (" + panX + "," + panY + ")" );
+        gl.uniform2f(_pan,panX,panY);
+        queue_draw();
+    }
 
-  var _position, _time, _zoom, _pan; // Attributes & uniforms
-  var program = null;
-  var choose_effect = function (frag) {
-      // console.log("choose_effect: " + frag);
-      // console.log("program == " + program);
-      /* Try to clean up from previous effect. I don't know whether
-         this step is necessary or sufficient. I still see the old
-         programs in Firefox's Shader Editor.
-      */
-      if (program) gl.deleteProgram(program);
-      program = gl.createProgram();
-      gl.attachShader(program, load_shader(gl,"shaders/image.vert"));
-      gl.attachShader(program, load_shader(gl,"shaders/"+frag+".frag"));
-      gl.linkProgram(program);
-      // Attributes & uniforms
-      _position = gl.getAttribLocation(program, "position");
-      _time = gl.getUniformLocation(program, "time");
-      if (!_time) console.log("non-animated");
-      _zoom = gl.getUniformLocation(program, "zoom");
-      _pan  = gl.getUniformLocation(program, "pan");
-      gl.enableVertexAttribArray(_position);
-      gl.useProgram(program);
-      panX = 0; panY = 0;
-      // Resize to set pan & zoom uniforms.
-      canvas.onresize();
-      tweak_pan(0,0);
-  };
+    var _position, _time, _zoom, _pan; // Attributes & uniforms
+    var program = null;
+    var choose_effect = function (frag) {
+        // console.log("choose_effect: " + frag);
+        // console.log("program == " + program);
+        /* Try to clean up from previous effect. I don't know whether
+           this step is necessary or sufficient. I still see the old
+           programs in Firefox's Shader Editor.
+        */
+        if (program) gl.deleteProgram(program);
+        program = gl.createProgram();
+        gl.attachShader(program, load_shader(gl,"shaders/image.vert"));
+        gl.attachShader(program, load_shader(gl,"shaders/"+frag+".frag"));
+        gl.linkProgram(program);
+        // Attributes & uniforms
+        _position = gl.getAttribLocation(program, "position");
+        _time = gl.getUniformLocation(program, "time");
+        if (!_time) console.log("non-animated");
+        _zoom = gl.getUniformLocation(program, "zoom");
+        _pan  = gl.getUniformLocation(program, "pan");
+        gl.enableVertexAttribArray(_position);
+        gl.useProgram(program);
+        panX = 0; panY = 0;
+        // Resize to set pan & zoom uniforms.
+        canvas.onresize();
+        tweak_pan(0,0);
+    };
 
-  var redraw = function(t) {
-      if (_time) {
-          gl.uniform1f(_time, t/1000);
-          queue_draw();
-      }
-      // gl.clear(gl.COLOR_BUFFER_BIT);
-      // gl.clear(0);
-      gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
-      gl.vertexAttribPointer(_position, 2, gl.FLOAT, false,4*2,0) ;
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
-      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-      // gl.flush();
-  };
-  var queue_draw = function() { window.requestAnimationFrame(redraw); }
-  var pixel_size; // in GL units
+    var redraw = function(t) {
+        if (_time) {
+            gl.uniform1f(_time, t/1000);
+            queue_draw();
+        }
+        // gl.clear(gl.COLOR_BUFFER_BIT);
+        // gl.clear(0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX);
+        gl.vertexAttribPointer(_position, 2, gl.FLOAT, false,4*2,0) ;
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        // gl.flush();
+    };
+    var queue_draw = function() { window.requestAnimationFrame(redraw); }
+    var pixel_size; // in GL units
 
-  canvas.onresize = function () {
-      var w = canvas.width, h = canvas.height;
-      // console.log("canvas size == ",[w,h]);
-      var mi = Math.min(w,h), ma = Math.max(w,h);
-      gl.viewport((w-ma)/2, (h-ma)/2, ma,ma);
-      gl.uniform1f(_zoom, mi/ma);
-      pixel_size = 2/ma;
-      // console.log("zoom == " + mi/ma + "; pixel_size == " + pixel_size);
-      queue_draw();
-  }
-  choose_effect(canvas.innerHTML);
-  // canvas.onresize();
-  // I tried event.movementX and event.movementY, but got undefined.
-  // jQuery might help.
-  var dragging = false;
-  var prevX, prevY;
-  canvas.onmousedown = function (event) {
-      dragging = true;
-      prevX = event.clientX; prevY = event.clientY;
-      // console.log("down"); 
-  };
-  canvas.onmouseup = function (event) { dragging = false; /* console.log("up"); */ };
-  canvas.onmousemove = function (event) {
-      if (dragging) {
-          var x = event.clientX, y = event.clientY;
-          var dx = x-prevX, dy= prevY-y;  // note y inversion
-          // console.log("delta = (" + dx + "," + dy + ")" );
-          tweak_pan(dx,dy);
-          prevX=x; prevY=y;
-      }
-  };
+    canvas.onresize = function () {
+        var w = canvas.width, h = canvas.height;
+        // console.log("canvas size == ",[w,h]);
+        var mi = Math.min(w,h), ma = Math.max(w,h);
+        gl.viewport((w-ma)/2, (h-ma)/2, ma,ma);
+        gl.uniform1f(_zoom, mi/ma);
+        pixel_size = 2/ma;
+        // console.log("zoom == " + mi/ma + "; pixel_size == " + pixel_size);
+        queue_draw();
+    }
+    // choose_effect(canvas.innerHTML);
+    choose_effect(effect);
+    // canvas.onresize();
+    // I tried event.movementX and event.movementY, but got undefined.
+    // jQuery might help.
+    var dragging = false;
+    var prevX, prevY;
+    canvas.onmousedown = function (event) {
+        dragging = true;
+        prevX = event.clientX; prevY = event.clientY;
+        // console.log("down"); 
+    };
+    canvas.onmouseup = function (event) { dragging = false; /* console.log("up"); */ };
+    canvas.onmousemove = function (event) {
+        if (dragging) {
+            var x = event.clientX, y = event.clientY;
+            var dx = x-prevX, dy= prevY-y;  // note y inversion
+            // console.log("delta = (" + dx + "," + dy + ")" );
+            tweak_pan(dx,dy);
+            prevX=x; prevY=y;
+        }
+    };
 };
